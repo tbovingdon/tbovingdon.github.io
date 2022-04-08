47bc7772f8188ef61b48071ee07e15ed30e4f468
tbovingdon <36049111+tbovingdon@users.noreply.github.com>
2022-04-08 07:35:40 -0400

    up

---
+++ wp-login.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+	<html lang="en-CA">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Lost Password &lsaquo; Infinity Network Solutions &#8212; WordPress</title>
+	<meta name='robots' content='noindex, follow' />
+<link rel='dns-prefetch' href='//www.google.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<script type="text/javascript">
+            window._nslDOMReady = function (callback) {
+                if ( document.readyState === "complete" || document.readyState === "interactive" ) {
+                    callback();
+                } else {
+                    document.addEventListener( "DOMContentLoaded", callback );
+                }
+            };
+            </script><script type='text/javascript' src='https://www.google.com/recaptcha/api.js?render=6LfANIYUAAAAAA2CfjQA-o3FS55tnUU6bDIK_NrE&#038;ver=5.9.3' id='wordfence-ls-recaptcha-js'></script>
+<script type='text/javascript' src='https://test.infinityns.ca/wp-includes/js/jquery/jquery.min.js' id='jquery-core-js'></script>
+<script type='text/javascript' src='https://test.infinityns.ca/wp-includes/js/jquery/jquery-migrate.min.js' id='jquery-migrate-js'></script>
+<script type='text/javascript' id='wordfence-ls-login-js-extra'>
+/* <![CDATA[ */
+var WFLSVars = {"ajaxurl":"https:\/\/test.infinityns.ca\/wp-admin\/admin-ajax.php","nonce":"80d774ea4f","recaptchasitekey":"6LfANIYUAAAAAA2CfjQA-o3FS55tnUU6bDIK_NrE","useCAPTCHA":"1","allowremember":"","verification":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://test.infinityns.ca/wp-content/plugins/wordfence/modules/login-security/js/login.1647958122.js' id='wordfence-ls-login-js'></script>
+<link rel='stylesheet' id='dashicons-css'  href='https://test.infinityns.ca/wp-includes/css/dashicons.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://test.infinityns.ca/wp-includes/css/buttons.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://test.infinityns.ca/wp-admin/css/forms.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://test.infinityns.ca/wp-admin/css/l10n.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://test.infinityns.ca/wp-admin/css/login.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='wordfence-ls-login-css'  href='https://test.infinityns.ca/wp-content/plugins/wordfence/modules/login-security/css/login.1647958122.css?ver=1.0.9' type='text/css' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://test.infinityns.ca/wp-content/uploads/2018/02/cropped-Infinity-symbol-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://test.infinityns.ca/wp-content/uploads/2018/02/cropped-Infinity-symbol-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://test.infinityns.ca/wp-content/uploads/2018/02/cropped-Infinity-symbol-180x180.png" />
+<meta name="msapplication-TileImage" content="https://test.infinityns.ca/wp-content/uploads/2018/02/cropped-Infinity-symbol-270x270.png" />
+	</head>
+	<body class="login no-js login-action-lostpassword wp-core-ui  locale-en-ca">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://en-ca.wordpress.org/">Powered by WordPress</a></h1>
+	<p class="message">Please enter your username or email address. You will receive an email message with instructions on how to reset your password.</p>
+
+		<form name="lostpasswordform" id="lostpasswordform" action="https://test.infinityns.ca/wp-login.php?action=lostpassword" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="user_login" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+						<input type="hidden" name="redirect_to" value="" />
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Get New Password" />
+			</p>
+		</form>
+
+		<p id="nav">
+			<a href="https://test.infinityns.ca/wp-login.php">Log in</a>
+					</p>
+				<p id="backtoblog">
+			<a href="https://test.infinityns.ca/">&larr; Go to Infinity Network Solutions</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" action="" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">Language</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="en_CA" lang="en" selected='selected' data-installed="1">English (Canada)</option></select>
+					
+					
+											<input type="hidden" name="action" value="lostpassword" />
+					
+						<input type="submit" class="button" value="Change">
+
+					</form>
+				</div>
+						<script type="text/javascript">
+		try{document.getElementById('user_login').focus();}catch(e){}
+		if(typeof wpOnload==='function')wpOnload();
+		</script>
+			<div class="clear"></div>
+	</body>
+	</html>
+	
